865f65c1ecafe5b5919881b899e5d03eeaf31e47
Peter Stanger <psta191@mywhitecliffe.com>
2020-06-01 13:16:20 +1200

    after multi dimensional array practice

--- src/arrayselfpractice/ArraySelfPractice.java
+++ src/arrayselfpractice/ArraySelfPractice.java
@@ -5,6 +5,8 @@
  */
 package arrayselfpractice;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Peter
@@ -15,6 +17,9 @@ public class ArraySelfPractice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        /*   1-Dimensional Arrays
+        
         String[] flavours = new String[4];
         
         flavours[0] = "chocolate";
@@ -33,6 +38,41 @@ public class ArraySelfPractice {
             numberArray[i]= i * 48;
             System.out.println(numberArray[i]);
         }
+        */
+        
+        
+        //  Two Dimensional Arrays
+        
+        double[][] studentGrades = {
+            {87.5, 77.1, 75.0},
+            {61.2, 67.3, 79.3},
+            {92.8, 92.8, 97.7},
+            {80.0, 75.9, 88.0}
+        };
+        
+        int boardDim = 8;
+        char[][] board = new char[boardDim][boardDim];
+        boolean isWhite = false;
+        for (int y = 0; y < board.length; y++) {
+            isWhite = !isWhite;
+            for (int x = 0; x < board[y].length; x++) {
+                
+                board[y][x] = isWhite ? 'W' : 'B';
+                isWhite = !isWhite;
+                System.out.print(board[y][x]);
+                
+            }
+            System.out.println("");
+        }
+
+        board[0][0] = 'K';
+        
+        for (int i = 0; i < board.length; i++) {
+            System.out.println(Arrays.toString(board[i]));
+        }
+        
+        System.out.print( board[4][3]);
+        System.out.print( board[0][0]);
         
     }
     
